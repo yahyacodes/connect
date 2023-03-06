98447cf635afca89c1f677c5d4c32f5996619318
yahyacodes <yahya.codin@gmail.com>
2023-03-07 00:25:33 +0300

    m

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-bold text-3xl underline">Hello Reactjs</h1>
+    </div>
+  );
+}
+
+export default App;
